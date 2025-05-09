b03e4df3391f1a58891f7d57b400eac7b9acce00
AlejG2005 <droiiidpm@hotmail.com>
2025-05-08 21:07:52 -0600

    estructura de back y front end

---
+++ backend/src/main/java/com/recomendaciones/recomendaciones/RecomendacionesApplication.java
@@ -0,0 +1,13 @@
+package com.recomendaciones.recomendaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendacionesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/recomendaciones/recomendaciones/RecomendacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recomendaciones.recomendaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
